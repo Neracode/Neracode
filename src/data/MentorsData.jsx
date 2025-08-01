78c015c115c9d89076c0af0a4c96e65b8c9f5cc5
rpl.aca <rasya.gani@smkn1kotabekasi.sch.id>
2025-08-01 14:59:52 +0700

    Update MentorsData.jsx

--- src/data/MentorsData.jsx
+++ src/data/MentorsData.jsx
@@ -1,6 +1,7 @@
 import { mentorImages } from 'src/assets';
 
 const MentorsData = [
+  // KETUA & WAKIL
   {
     name: 'Fachri',
     posisi: 'Ketua Neracode',
@@ -8,17 +9,19 @@ const MentorsData = [
     img: mentorImages.mentor1,
   },
   {
-    name: 'Reza',
-    posisi: 'Wakil Neracode',
+    name: 'Prince',
+    posisi: 'Wakil Ketua 1 Neracode',
     angkatan: 'angkatan 28',
     img: mentorImages.mentor1,
   },
   {
-    name: 'Anissa',
-    posisi: 'Wakil Neracode',
+    name: 'Annisa',
+    posisi: 'Wakil Ketua 2 Neracode',
     angkatan: 'angkatan 28',
     img: mentorImages.mentor1,
   },
+
+  // MENTOR
   {
     name: 'Taufik',
     posisi: 'Mentor Neracode',
@@ -43,28 +46,130 @@ const MentorsData = [
     angkatan: 'angkatan 27',
     img: mentorImages.mentor1,
   },
+
+  // SEKRETARIS
   {
-    name: '-',
-    posisi: 'Ketua Neracode',
-    angkatan: 'angkatan 26',
+    name: 'Nuha',
+    posisi: 'Sekretaris 1 Neracode',
+    angkatan: 'angkatan 28',
     img: mentorImages.mentor1,
   },
   {
-    name: '-',
-    posisi: 'Ketua Neracode',
-    angkatan: 'angkatan 26',
+    name: 'Nafisah',
+    posisi: 'Sekretaris 2 Neracode',
+    angkatan: 'angkatan 28',
+    img: mentorImages.mentor1,
+  },
+
+  // BENDAHARA
+  {
+    name: 'Nela',
+    posisi: 'Bendahara 1 Neracode',
+    angkatan: 'angkatan 28',
     img: mentorImages.mentor1,
   },
   {
-    name: '-',
-    posisi: 'Ketua Neracode',
-    angkatan: 'angkatan 26',
+    name: 'Mira',
+    posisi: 'Bendahara 2 Neracode',
+    angkatan: 'angkatan 28',
+    img: mentorImages.mentor1,
+  },
+
+  // K3 & LOGISTIK
+  {
+    name: 'Fikri',
+    posisi: 'Tim K3 & Logistik',
+    angkatan: 'angkatan 28',
     img: mentorImages.mentor1,
   },
   {
-    name: '-',
-    posisi: 'Ketua Neracode',
-    angkatan: 'angkatan 26',
+    name: 'Syarif',
+    posisi: 'Tim K3 & Logistik',
+    angkatan: 'angkatan 28',
+    img: mentorImages.mentor1,
+  },
+  {
+    name: 'Abdul',
+    posisi: 'Tim K3 & Logistik',
+    angkatan: 'angkatan 28',
+    img: mentorImages.mentor1,
+  },
+  {
+    name: 'Dzakky',
+    posisi: 'Tim K3 & Logistik',
+    angkatan: 'angkatan 28',
+    img: mentorImages.mentor1,
+  },
+  {
+    name: 'Andika',
+    posisi: 'Tim K3',
+    angkatan: 'angkatan 28',
+    img: mentorImages.mentor1,
+  },
+  {
+    name: 'Tengku',
+    posisi: 'Tim K3',
+    angkatan: 'angkatan 28',
+    img: mentorImages.mentor1,
+  },
+
+  // HUMAS
+  {
+    name: 'Resti',
+    posisi: 'Tim Humas',
+    angkatan: 'angkatan 28',
+    img: mentorImages.mentor1,
+  },
+
+  // EVENT
+  {
+    name: 'Razif',
+    posisi: 'Tim Event',
+    angkatan: 'angkatan 28',
+    img: mentorImages.mentor1,
+  },
+  {
+    name: 'Hibban',
+    posisi: 'Tim Event',
+    angkatan: 'angkatan 28',
+    img: mentorImages.mentor1,
+  },
+
+  // MEDIA
+  {
+    name: 'Najla',
+    posisi: 'Tim Media',
+    angkatan: 'angkatan 28',
+    img: mentorImages.mentor1,
+  },
+  {
+    name: 'Kentaro',
+    posisi: 'Tim Media',
+    angkatan: 'angkatan 28',
+    img: mentorImages.mentor1,
+  },
+  {
+    name: 'Axel',
+    posisi: 'Tim Media',
+    angkatan: 'angkatan 28',
+    img: mentorImages.mentor1,
+  },
+  {
+    name: 'Melda',
+    posisi: 'Tim Media',
+    angkatan: 'angkatan 28',
+    img: mentorImages.mentor1,
+  },
+  {
+    name: 'Altha',
+    posisi: 'Tim Media',
+    angkatan: 'angkatan 28',
+    img: mentorImages.mentor1,
+  },
+  {
+    name: 'Nay',
+    posisi: 'Tim Media',
+    angkatan: 'angkatan 28',
     img: mentorImages.mentor1,
   },
 ];
